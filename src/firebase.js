9ed7c662863db3cdc5bf76340ac0f1efbf38b8c5
dasnishant1999 <dasnishant1999@gmail.com>
2021-04-22 18:02:37 +0530

    firebase-keys

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBAXhkwOtmU_wttmnxeEa1gh9C9UIs56WE",
+  authDomain: "authentication-5f256.firebaseapp.com",
+  projectId: "authentication-5f256",
+  storageBucket: "authentication-5f256.appspot.com",
+  messagingSenderId: "948197483586",
+  appId: "1:948197483586:web:ffeede08d4373ce48ff9db",
 });
 
 export const auth = firebase.auth();
